693f4011849a3067809ac12a45d8315393d91d80
Tom Montgomery <montgomery3681@gmail.com>
2023-11-04 23:13:10 -0600

    Initial Commit

---
+++ Mapkit_Udemy/Mapkit_UdemyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mapkit_UdemyApp.swift
+//  Mapkit_Udemy
+//
+//  Created by tom montgomery on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Mapkit_UdemyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
